fd719deab50e78b74dca4c4fec3bdcbd353a228d
Thomas Gustafson <gus.duckfan@gmail.com>
2024-01-22 17:40:12 -0800

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
+const add = function(num1, num2) {
+	return num1+num2;
+};
+
+const subtract = function(num1, num2) {
+	return num1-num2;
+};
+
+const sum = function(numArray) {
+  let result = 0;
+  numArray.forEach(num =>{
+    result+=parseInt(num);
+  })
+  return parseInt(result);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const multiply = function(numArray) {
+  let result = 1;
+  for(const num of numArray){
+    result*=parseInt(num);
+  }
+  return result;
 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for(let i=2; i<=num; i++){
+    result*=i;
+  }
+  return result;
 };
 
 // Do not edit below this line
