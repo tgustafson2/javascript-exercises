ddc2b345311e714f585548e0af8b70aa444bc470
Thomas Gustafson <gus.duckfan@gmail.com>
2024-01-22 19:02:43 -0800

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce(
+        (oldestPerson, currentPerson) => {
+            if(!oldestPerson.hasOwnProperty("yearOfDeath")){
+                const date = new Date();
+                oldestPerson.yearOfDeath = date.getFullYear();
+            }
+            if(!currentPerson.hasOwnProperty("yearOfDeath")){
+                const date = new Date();
+                currentPerson.yearOfDeath = date.getFullYear();
+            }
+            if(currentPerson.yearOfDeath-currentPerson.yearOfBirth>
+                oldestPerson.yearOfDeath-oldestPerson.yearOfBirth){
+                    oldestPerson=currentPerson;
+                }
+            return oldestPerson;
+        
+    },people[0]);
 
 };
 
