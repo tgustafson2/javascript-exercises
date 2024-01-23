d572e7c2efc1ac9a7aca2db6863f3874874bb0b6
Thomas Gustafson <gus.duckfan@gmail.com>
2024-01-22 18:04:32 -0800

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(targetIdx) {
+    let arr = new Array(1,1);
+    targetIdx = parseInt(targetIdx);
+    if(targetIdx<=0){
+        return targetIdx==0 ? 0 : "OOPS";
+    }
+    for(let i=2; i<targetIdx; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[arr.length-1]
 };
 
 // Do not edit below this line
