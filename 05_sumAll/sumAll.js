ba07ae9ee30612d1ed7d9040bc8d737874be1a34
Thomas Gustafson <gus.duckfan@gmail.com>
2024-01-22 17:19:50 -0800

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(typeof(num1)!="number" || typeof(num2)!="number" || num1<0 || num2<0){
+        return "ERROR";
+    }
+    if(num1>num2){
+        return arguments.callee(num2,num1);
+    }
+    let result = 0;
+    for( let i=num1; i<=num2; i++){
+        result+=i;
+    }
+    return result;
+    
 };
 
 // Do not edit below this line
