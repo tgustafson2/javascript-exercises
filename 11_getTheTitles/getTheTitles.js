fddd95aa9a38942a48c2a6487cbe43dd134cace6
Thomas Gustafson <gus.duckfan@gmail.com>
2024-01-22 18:06:40 -0800

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArray) {
+    return bookArray.map(book =>{
+        return book.title;
+    })
 };
 
 // Do not edit below this line
