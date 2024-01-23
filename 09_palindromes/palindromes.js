9cae4694a246333c7d3eff576a891706fc06229c
Thomas Gustafson <gus.duckfan@gmail.com>
2024-01-22 17:56:23 -0800

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let left = 0;
+    let right = str.length -1;
+    let leftChar, rightChar;
+    while(left<right){
+        leftChar = str[left];
+        rightChar = str[right];
+        if(!/[a-zA-Z0-9]/.test(leftChar)){
+            left++;
+        }
+        else if(!/[a-zA-Z0-9]/.test(rightChar)){
+            right--;
+        }
+        else{
+            if(leftChar.toLowerCase()!==rightChar.toLowerCase()){
+                return false;
+            }
+            left++;
+            right--;
+        }
+    }
+    return true;
 
 };
 
