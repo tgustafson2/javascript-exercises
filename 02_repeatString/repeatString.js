e85325a8a0ee6cd8690a10b8bd5e0187f57d3b18
Thomas Gustafson <gus.duckfan@gmail.com>
2024-01-22 16:03:38 -0800

    Finish Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(strToRepeat, numOfRepeats) {
+    if(numOfRepeats<0)return "ERROR";
+    let result = "";
+    for(let i=0; i<numOfRepeats; i++){
+        result+=strToRepeat;
+    }
+    return result;
 };
 
 // Do not edit below this line
